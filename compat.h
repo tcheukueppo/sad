2e913b1970cc12bfb648ff840638a0cde4e63463
sin <sin@2f30.org>
2014-12-27 18:26:51 +0000

    Remember to undef strtonum

--- compat.h
+++ compat.h
@@ -6,4 +6,5 @@ size_t strlcat(char *, const char *, size_t);
 #undef strlcpy
 size_t strlcpy(char *, const char *, size_t);
 
+#undef strtonum
 long long strtonum(const char *, long long, long long, const char **);
