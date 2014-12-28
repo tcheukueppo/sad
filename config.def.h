d8f9ff644b9bd7c5d40e40f60e81d4c822e16e1e
sin <sin@2f30.org>
2014-12-28 15:40:46 +0000

    Initialize the struct properly in config.def.h

--- config.def.h
+++ config.def.h
@@ -1,4 +1,4 @@
 Outputdesc Outputdescs[] = {
-       { "sndio", 16, 44100, 2, 1, &sndiooutput },
-       { "fifo",  16, 44100, 2, 1, &fifooutput  },
+       { "sndio", 16, 44100, 2, 1, 0, &sndiooutput },
+       { "fifo",  16, 44100, 2, 1, 0, &fifooutput  },
 };
