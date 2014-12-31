1ee6ab56c0c39d65f74eb614d0cf552a3ebc5b5d
sin <sin@2f30.org>
2014-12-31 22:19:59 +0000

    Style fix

--- output.c
+++ output.c
@@ -156,7 +156,7 @@ s16monotostereo(short *in, short *out, size_t nsamples)
 {
 	size_t i;
 
-	for (i = 0; i < nsamples; ++i) {
+	for (i = 0; i < nsamples; i++) {
 		out[i * 2] = in[i];
 		out[i * 2 + 1] = in[i];
 	}
