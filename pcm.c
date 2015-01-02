5e0e896f3a45a3351c55d36c60724db4804c3534
sin <sin@2f30.org>
2015-01-02 14:51:36 +0000

    Simplify expression

--- pcm.c
+++ pcm.c
@@ -22,5 +22,5 @@ s16stereotomono(short *in, short *out, size_t nsamples)
 	size_t i;
 
 	for (i = 0; i < nsamples; i++)
-		out[i] = (in[i * 2] / 2) + (in[i * 2 + 1] / 2);
+		out[i] = in[i * 2] / 2 + in[i * 2 + 1] / 2;
 }
